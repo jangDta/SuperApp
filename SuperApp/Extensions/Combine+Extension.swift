5a4b9069a0c58661a4b16e9dc0ccb3a281671501
jangDta <dnwkqk@gmail.com>
2021-12-23 15:27:04 +0900

    CombineExt 패키지 적용 CurrentValuePublisher 생성 (Relay를 ReadOnly하게 만들어 값을 받을수만 있고 받은 값을 DownStream으로 보내줄수 있는 클래스)

---
+++ SuperApp/Extensions/Combine+Extension.swift
@@ -0,0 +1,45 @@
+//
+//  Combine+Extension.swift
+//  SuperApp
+//
+//  Created by a60105114 on 2021/12/23.
+//
+
+import Combine
+import CombineExt
+import Foundation
+
+public class ReadOnlyCurrentValuePublisher<Element>: Publisher {
+    
+    public typealias Output = Element
+    public typealias Failure = Never
+    
+    public var value: Element {
+        currentValueRelay.value
+    }
+    
+    fileprivate let currentValueRelay: CurrentValueRelay<Output>
+    
+    fileprivate init(_ initialValue: Element) {
+        currentValueRelay = CurrentValueRelay(initialValue)
+    }
+    
+    public func receive<S>(subscriber: S) where S : Subscriber, Never == S.Failure, Element == S.Input {
+        currentValueRelay.receive(subscriber: subscriber)
+    }
+}
+
+public final class CurrentValuePublisher<Element>: ReadOnlyCurrentValuePublisher<Element> {
+    
+    public typealias Output = Element
+    public typealias Failure = Never
+    
+    override init(_ initialValue: Element) {
+        super.init(initialValue)
+    }
+    
+    public func send(_ value: Element) {
+        currentValueRelay.accept(value)
+    }
+    
+}
