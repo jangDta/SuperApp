fc1d52957061eae1f93822fa3c00bba98e11be57
jangDta <dnwkqk@gmail.com>
2022-01-03 17:10:49 +0900

    Package 추가
    * CX, Finance, Transport, Profile, Platform package 추가

---
+++ CX/Sources/CX/CX.swift
@@ -0,0 +1,6 @@
+public struct CX {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Transport/Sources/Transport/Transport.swift
@@ -0,0 +1,6 @@
+public struct Transport {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
